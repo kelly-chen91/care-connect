bff0fd5382a76ebee986c270fb76f81489df3e02
Kelly Chen <kellychen966@gmail.com>
2024-02-03 12:06:32 -0500

    profile team

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
     </>
   )
 }
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+import { getImageUrl } from '../../utils'
+
+function About()
+{
+    <ProfileList/>
+}
+
+function ProfilePic({person,size})
+{
+    <img
+        className='profilePic'
+        src = {getImageUrl(person)}
+        alt = {person.name}
+        width = {size}
+        height = {size}
+    />
+}
+
+export default function ProfileList()
+{
+    return (
+        <div>
+            <ProfilePic 
+                person={{
+                    name: 'Jane Doe',
+                    imageId: 'emoji.png'
+                }}
+            />
+            <ProfilePic 
+                person={{
+                    name: 'JaneDoe',
+                    imageId: 'emoji.png'
+                }}
+            />
+        </div>
+    );
+}
