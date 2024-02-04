d21b2a17c013b49f0a83ec8f29b135fd8f74998f
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-02-03 20:56:12 -0500

    minor syntax fix
    added semicolon

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import Layout from "./components/Layout";
 import Home from "./components/Home";
 import AboutPage from "./components/aboutPage";
 import Error from "./components/Error";
-import Portal from "./components/Portal.jsx"
+import Portal from "./components/Portal.jsx";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -23,15 +23,11 @@ function App() {
             <Route path="about" element={<AboutPage />} />
             <Route path="portal" element={<Portal />} />
             <Route path="*" element={<Error />} />
-
           </Route>
         </Routes>
         {/* <Outlet /> */}
-
       </BrowserRouter>
     </>
-
-
   );
 }
 
