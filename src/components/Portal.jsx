525591d92e59731e937ad1544e8d0f1b3b6f95a6
xyz1236 <linmaggie50@gmail.com>
2024-02-03 23:04:22 -0500

    bug

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
+import "./portal.css";
 
 export default function Portal() {
-    const [user, setUser] = useState("Guest")
+  const [user, setUser] = useState("Guest");
 
-    useEffect(() => {
-        setUser(window.localStorage.getItem("user"))
-    }, [])
-    return <div class="text-center w-screen">
-        <h1 >Hello {user}</h1>
-    </div>;
+  useEffect(() => {
+    setUser(window.localStorage.getItem("user"));
+  }, []);
+  return (
+    <div className="username">
+      <h1>Hello {user}</h1>
+    </div>
+  );
 }
