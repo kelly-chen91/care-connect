74f9602bbccec6dd55528a18a46c1d01cb9d5b43
Kelly Chen <kellychen966@gmail.com>
2024-02-04 14:11:43 -0500

    working on portal

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -4,6 +4,7 @@ import '../portal.css'
 import cna_shift from '../assets/cna_shift.jpeg'
 import notifications from '../assets/notifications.jpeg'
 import calendar from '../assets/calendar.jpeg'
+import nurse from "../assets/nurse.png"
 
 export default function Portal() {
     const [user, setUser] = useState("Guest")
@@ -14,21 +15,43 @@ export default function Portal() {
     return (
         <div>
             <h1 className="username">Hello {user}</h1>
-            <div className="flex items-center justify-between px-12">
+            <div className="flex items-center justify-center px-12">
                 <div>
-                    <img
+                    {/* <img
                         className='w-4/5'
                         src={cna_shift}
                         alt='Jane Smith'
-                    />
+                    /> */}
+                    <h2 className="text-4xl text-center">CNA of the Shift</h2>
+                    <div className="flex items-center justify-center">
+                        <div className="text-center text-2xl">
+                            <p>Name: Jennifer Smith</p>
+                            <p>Hours: 7:00AM to 3:30PM</p>
+                            <p>123-123-1234</p>
+                        </div>
+                        <img
+                            className='w-4/12 rounded-full'
+                            src={nurse}
+                            alt='Jane Smith'
+                        />
+                    </div>
+
                 </div>
                 <div>
-                    <img
+                    <div className="flex flex-col bg-indigo-200 rounded-3xl p-4 w-80 text-center">
+                        {/* <img
                         className='w-4/5'
                         src={notifications}
                         alt='notification'
-                    />
+                    /> */}
+                        <p className="text-2xl">Notifications</p>
+                        <div className="bg-white rounded-3xl p-4 m-2"> Col 1 </div>
+                        <div className="bg-white rounded-3xl p-4 m-2"> Col 2 </div>
+                        <div className="bg-white rounded-3xl p-4 m-2"> Col 3 </div>
+                        <div className="bg-white rounded-3xl p-4 m-2"> Col 4 </div>
+                    </div>
                 </div>
+
 
 
             </div>
